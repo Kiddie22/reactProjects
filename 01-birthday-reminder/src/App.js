5ce67ddaf6e9ab9ef422eb1279215107799d7476
Kiddie22 <rasula.20191118@iit.ac.lk>
2021-04-27 19:03:26 +0530

    finalized 01-birthday-reminder

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 
-function App() {
+const App = () => {
+  const [people, setPeople] = useState(data);
+
+  const clearAll = () => {
+    setPeople([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <section className="container">
+          <h3>{people.length} birthdays today</h3>
+          {people.map((person) => {
+            return <List key={person.id} person={person} />;
+          })}
+          <button onClick={clearAll}> Clear All </button>
+        </section>
+      </main>
+    </>
   );
-}
+};
 
 export default App;
---
+++ 01-birthday-reminder/src/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const List = (props) => {
+  const { id, name, age, image } = props.person;
+  return (
+    <>
+      <article className="person">
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default List;
