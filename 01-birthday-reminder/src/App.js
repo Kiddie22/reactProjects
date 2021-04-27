fef88032970cfbedf1bf8437d485a0c603c0370c
Kiddie22 <rasula.20191118@iit.ac.lk>
2021-04-27 19:08:06 +0530

    added reset button

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -4,9 +4,16 @@ import List from "./List";
 
 const App = () => {
   const [people, setPeople] = useState(data);
+  const [isEmpty, setIsEmpty] = useState(false);
 
   const clearAll = () => {
     setPeople([]);
+    setIsEmpty(true);
+  };
+
+  const reset = () => {
+    setPeople(data);
+    setIsEmpty(false);
   };
 
   return (
@@ -17,7 +24,8 @@ const App = () => {
           {people.map((person) => {
             return <List key={person.id} person={person} />;
           })}
-          <button onClick={clearAll}> Clear All </button>
+          {!isEmpty && <button onClick={clearAll}> Clear All </button>}
+          {isEmpty && <button onClick={reset}> Reset </button>}
         </section>
       </main>
     </>
