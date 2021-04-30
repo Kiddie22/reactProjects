019b7c86090e9e5055d12e827f10616219439f52
Kiddie22 <rasula.20191118@iit.ac.lk>
2021-04-30 17:47:28 +0530

    added switching between reviews

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -3,33 +3,45 @@ import reviews from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
+  const [index, setIndex] = useState(0);
+  const currentReview = reviews[index];
+
+  const nextReview = () => {
+    const tempIndex = index + 1;
+    tempIndex > reviews.length - 1 ? setIndex(0) : setIndex(tempIndex);
+  };
+
+  const previousReview = () => {
+    const tempIndex = index - 1;
+    tempIndex < 0 ? setIndex(reviews.length - 1) : setIndex(tempIndex);
+  };
+
   return (
     <>
-      {reviews.map((review) => {
-        const { id, name, job, image, text } = review;
-        return (
-          <article className="review">
-            <div className="img-container">
-              <img className="person-img" src={image} alt={name} />
-              <span className="quote-icon">
-                <FaQuoteRight />
-              </span>
-            </div>
-            <h4 className="author">{name}</h4>
-            <p className="job">{job}</p>
-            <p className="info">{text}</p>
-            <div className="button-container">
-              <button className="prev-btn">
-                <FaChevronLeft />
-              </button>
-              <button className="next-btn">
-                <FaChevronRight />
-              </button>
-            </div>
-            <button className="random-btn">surprise me</button>
-          </article>
-        );    
-      })}
+      <article className="review">
+        <div className="img-container">
+          <img
+            className="person-img"
+            src={currentReview.image}
+            alt={currentReview.name}
+          />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{currentReview.name}</h4>
+        <p className="job">{currentReview.job}</p>
+        <p className="info">{currentReview.text}</p>
+        <div className="button-container">
+          <button className="prev-btn" onClick={() => previousReview()}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={() => nextReview()}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn">surprise me</button>
+      </article>
     </>
   );
 };
