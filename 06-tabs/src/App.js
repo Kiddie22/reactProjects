c15fdfaf9fe5d8442e3303cd5083301e251fa68c
Kiddie22 <rasula.20191118@iit.ac.lk>
2021-05-02 19:22:40 +0530

    added showing job details

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -4,8 +4,9 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([{}]);
   const [isLoading, setIsLoading] = useState(true);
+  const [order, setOrder] = useState(0);
 
   useEffect(() => {
     fetch(url)
@@ -14,23 +15,42 @@ const App = () => {
     setIsLoading(false);
   }, []);
 
+  const { title, company, dates, duties } = data[order];
+
   return (
-    <main>
-      <div className="section">
-        {isLoading && <div className="loading">Loading...</div>}
-        {!isLoading && (
-          <>
-            <div className="title">
-              <h2>Experience</h2>
-              <div className="underline"></div>
+    <div className="section">
+      {isLoading && <div className="loading">Loading...</div>}
+      {!isLoading && (
+        <>
+          <div className="title">
+            <h2>Experience</h2>
+            <div className="underline"></div>
+          </div>
+
+          {/* Jobs navbar */}
+          <div className="jobs-center">
+            <div className="btn-container">
+              {data.map((job) => (
+                <button className="job-btn">{job.company}</button>
+              ))}
             </div>
-            {data.map((job) => {
-              return job.title;
-            })}
-          </>
-        )}
-      </div>
-    </main>
+
+            {/* Job info */}
+            <div className="job-info">
+              <h3>{title}</h3>
+              <h4>{company}</h4>
+              <div className="job-date">{dates}</div>
+              {/* {duties.map((duty) => (
+                <div className="job-desc">
+                  <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                  <p>{duty}</p>
+                </div>
+              ))} */}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
